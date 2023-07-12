5b898c074ce2bc0d3b52c5a972551016d2fc8856
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-07-12 11:07:02 +0900

    feat: toggleName

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,9 +1,17 @@
 <script>
 	export let name;
+	function toggleName() {
+		if(name === 'world') {
+			name = "svelte"
+		} else {
+			name = "world"
+		}
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<button on:click={toggleName}>Toggle button</button>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
